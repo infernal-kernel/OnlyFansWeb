8d5388a624bc9db662c72dab94b5b660cf57d76c
infernal-kernel <infernalhacking@gmail.com>
2025-03-18 23:57:49 +0000

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // Contador regresivo de 24 horas
 function countdown() {
-    // Verificamos si ya existe un tiempo de expiración guardado en el localStorage
     let targetDate = localStorage.getItem('targetDate');
 
     if (!targetDate) {
-        // Si no existe, definimos el tiempo de expiración 24 horas desde ahora
         targetDate = new Date();
         targetDate.setHours(targetDate.getHours() + 24);
-        localStorage.setItem('targetDate', targetDate); // Guardamos el tiempo de expiración en localStorage
+        localStorage.setItem('targetDate', targetDate);
     } else {
-        targetDate = new Date(targetDate); // Convertimos el valor guardado en un objeto Date
+        targetDate = new Date(targetDate);
     }
 
     function updateTimer() {
@@ -18,7 +16,7 @@ function countdown() {
 
         if (difference < 0) {
             document.getElementById("timer").innerHTML = "¡La oferta ha terminado!";
-            clearInterval(timerInterval); // Detenemos el contador cuando termina
+            clearInterval(timerInterval);
             return;
         }
 
@@ -29,10 +27,8 @@ function countdown() {
         document.getElementById("timer").innerHTML = `${hours}h ${minutes}m ${seconds}s`;
     }
 
-    // Llamamos a la función para actualizar el contador cada segundo
     let timerInterval = setInterval(updateTimer, 1000);
 
-    // Actualizamos el contador inmediatamente al cargar
     updateTimer();
 }
 
