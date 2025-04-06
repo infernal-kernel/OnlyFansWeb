43759b4bbd2c18c4f9e6f92e905a9e1c0ba54eb5
infernal-kernel <infernalhacking@gmail.com>
2025-04-06 06:34:14 +0200

    script.js

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ function countdown() {
     }
 }
 setInterval(countdown, 1000);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('✅ Service Worker registrado:', reg.scope))
+      .catch(err => console.error('❌ Error al registrar el Service Worker:', err));
+  });
+}
+
