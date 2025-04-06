15da58e9151acd137fb6dde0776d2f29e2e549e9
infernal-kernel <infernalhacking@gmail.com>
2025-04-06 06:33:47 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'onlyfans-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap',
+  'https://upload.wikimedia.org/wikipedia/commons/6/6e/OnlyFans_logo_2021.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
